56a723f1577351cd0f285f11c729f4642c3363ad
Rafael Marques <rfmapp@gmail.com>
2017-05-22 17:45:08 -0300

    Arrays lessons

---
+++ section6/app11Arrays.rb
@@ -0,0 +1,7 @@
+array1 = [1,2,3]
+array2 = Array.new(3, "hello")
+array3 = [1, "hello", [2,3,4]]
+
+p(array1)
+p(array2)
+p(array3)
---
+++ section6/app12ArraysIndexingIterating.rb
@@ -0,0 +1,15 @@
+array = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+array2 = [1,2,3,['a', 'b', 'c']]
+
+print(array[0, 5])
+puts
+print(array[-5, 5])
+puts
+print(array[0..4])
+puts
+print(array[-5..-1])
+puts
+
+for x in array2
+  p(x)
+end
